ac23045039aea2186aaaab5f002739ee2438fc27
ForhadRoben <i.rforhad@gmail.com>
2025-02-15 11:30:54 +0600

    cors commented

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,5 @@
 import express, { Application, Request, Response } from 'express';
-import cors from 'cors';
+// import cors from 'cors';
 import { StudentRoutes } from './app/modules/student.route';
 const app: Application = express();
 
@@ -7,7 +7,7 @@ const app: Application = express();
 
 // parsers
 app.use(express.json());
-app.use(cors());
+// app.use(cors());
 
 // application routes
 app.use('/api/v1/students', StudentRoutes);
